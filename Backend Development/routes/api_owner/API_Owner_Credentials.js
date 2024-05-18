04fba4b923c3c957847c9ca584df8764846e64d5
Mark01 <115213441+Mark01-1104@users.noreply.github.com>
2024-05-18 20:56:32 +0800

    Add files via upload

--- Backend Development/routes/api_owner/API_Owner_Credentials.js
+++ Backend Development/routes/api_owner/API_Owner_Credentials.js
@@ -4,36 +4,41 @@ var router = express.Router();
 
 var bcrypt = require('bcrypt');
 
+var jwt_token = require("jsonwebtoken");
+
 var dbConn = require('../../config/db.js');
 
 
 
 //INSERT
 router.post('/Staff/Signup', async (req, res) => {
-    var salt_rounds = 10;
+  var salt_rounds = 10;
 
-    var Account_Id = req.body.Account_Id;
-    var Email = req.body.Email;
-    var Username = req.body.Username;
-    var Password = req.body.Password;
-    var Name = req.body.Name;
-    var Role = req.body.Role;
+  var Account_Id = req.body.Account_Id;
+  var Email = req.body.Email;
+  var Username = req.body.Username;
+  var Password = req.body.Password;
+  var Name = req.body.Name;
+  var Role = req.body.Role;
 
-    const hash_password = await bcrypt.hash(Password,parseInt(salt_rounds))
+  const hash_password = await bcrypt.hash(Password, parseInt(salt_rounds));
 
-
-    try{
-        sqlQuery = `INSERT INTO accounts(Account_Id,Email,Username,Password,Name,Role) VALUES (?, ?, ?, ?, ?, ?)`
-        dbConn.con1.query(sqlQuery, [Account_Id, Email, hash_password, Username, Name, Role], function(error, results, fields){
-            console.log(results);
-        });
-    }
-    catch(error){
-        console.log(error);
-        return next (error);
-    }
-   res.status(200).json({success: true});   
+  try {
+      const sqlQuery = `INSERT INTO accounts(Account_Id, Email, Username, Pwd, Name, Role) VALUES (?,?,?,?,?,?)`;
+      dbConn.con1.query(sqlQuery, [Account_Id, Email,  Username,hash_password, Name, Role], function (error, results, fields) {
+          if (error) {
+              console.log(error);
+              return next(error);
+          }
+          console.log(results);
+          res.status(200).json({ success: true });
+      });
+  } catch (error) {
+      console.log(error);
+      return next(error);
+  }
 });
+
 
 // SELECT or (VIEW)
 router.get('/View/Account', async (req, res) => {
@@ -65,6 +70,66 @@ router.get('/View/Account', async (req, res) => {
     } catch (error) {
       console.log('Error caught', error);
       res.status(500).json({ success: false, message: "Error, invalid JWT token" });
+    }
+  });
+
+  // UPDATE
+  router.post('/Staff/Update', async (req, res) => {
+    var salt_rounds = 10;
+
+  var Account_Id = req.body.Account_Id;
+  var Email = req.body.Email;
+  var Username = req.body.Username;
+  var Password = req.body.Password;
+  var Name = req.body.Name;
+  var Role = req.body.Role;
+
+  const hash_password = await bcrypt.hash(Password, parseInt(salt_rounds));
+
+  try {
+      const sqlQuery = `UPDATE INTO accounts Email = "${Email}", Username = "${Username}", Pwd = "${hash_password}", Name = "${Name}", Role = "${Role}" WHERE Account_Id = ${Account_Id}`;
+      dbConn.con1.query(sqlQuery, [Account_Id, Email,  Username,hash_password, Name, Role], function (error, results, fields) {
+          if (error) {
+              console.log(error);
+              return next(error);
+          }
+          console.log(results);
+          res.status(200).json({ success: true });
+      });
+  } catch (error) {
+      console.log(error);
+      return next(error);
+  }
+});
+
+  // Delete
+  router.delete('/Delete/Account', (req, res) => {
+
+    const Pass = req.headers.authorization.split(' ')[1];
+  
+    if (!Pass) {
+        console.log('JWT token not found');
+        return res.status(401).json({ success: false, message: "Error, JWT token not found" });
+    }
+  
+    try {
+        console.log('Decoding JWT token');
+        const decodedToken = jwt_token.verify(Pass,process.env.Special_Token);
+  
+        var Account_Id = req.body.Account_Id;
+  
+        sqlQuery = `DELETE FROM accounts WHERE Account_Id = ${Account_Id}`;
+  
+        dbConn.con1.query(sqlQuery, function(error, results, fields){
+            if (error) {
+                console.log('Error executing query', error);
+                return res.status(401).json({ success: false, message: "Error, invalid JWT token" });
+            }
+            res.status(200).json(results);
+        });
+    } catch (error) {
+        console.log('Error caught', error);
+        return res.status(401).json({ success: false, message: "Error, invalid JWT token" });
     }
   });
 
